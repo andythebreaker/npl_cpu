8e4321721a529f3a6abd3796ed1f9ddff31f11fe
andythebreaker <43373581+andythebreaker@users.noreply.github.com>
2019-12-26 23:23:20 +0800

    負數錯誤

--- cpu.v
+++ cpu.v
@@ -9,10 +9,10 @@ module instruction_set_model(
     output [0:31] MEM_OUT,/*0:WIDTH-1*/
     output MEM_CTRL,//0:read | 1:write
     output [11:0] INS_ADDR,
-    input [0:31] INS_MEM//always in read mode
+    input [0:31] INS_MEM,//always in read mode
 
     //-------------------
-    //output [32:0] reg_debug_out
+    output [32:0] reg_debug_out
     //-------------------
 );
 //parameter CYCLE = 10;//cycle time
@@ -104,7 +104,8 @@ IR[11:0] : destination address
 integer i;
 
 //--------------------------------------------
-//assign reg_debug_out = ir;
+reg [32:0] reg_debug_o;
+assign reg_debug_out = reg_debug_o;
 //--------------------------------------------
 
 //function
@@ -167,6 +168,9 @@ begin//always @(posedge clk or posedge rst)
         MEM_O = 0;
         temp_checkcond = 0;
         MEM_C = 0;
+        //-------------------
+        reg_debug_o = 0;
+        //-------------------
  
     end else begin
         //make mem. unreadable
@@ -268,6 +272,21 @@ begin//always @(posedge clk or posedge rst)
                 psr = 0;//clearcondcode
                 src1 = getsrc(ir);
                 src2 = getdst(ir);
+                if (~src1[ADDRSIZE-1/*:0*/]/*>=0*//*right*/) begin
+                    //result[WIDTH-1:WIDTH-1-src1[ADDRSIZE-1:0]]=src2[0:src1[ADDRSIZE-1:0]-1];
+                    result = ( src2 >> src1[ADDRSIZE-1:0] ) | ( src2 << ( WIDTH -src1[ADDRSIZE-1:0] ) );
+                end else begin/*left*/
+                    //bit_rotate_left
+                    //Bitwise or not equal to||
+                    result[32]=0;
+                    result[31:0] = ( src2 << ( -src1[ADDRSIZE-1:0] ) ) | ( src2 >> ( WIDTH -( -src1[ADDRSIZE-1:0] ) ) );
+                    //result=33'b000000000000000000000000000000001;
+                    //debug_r=0;
+                end
+                debug_r = setcondcode(result);
+                    //debug_r=(src1[ADDRSIZE-1:0])?1:0;
+                    //reg_debug_o=( src2 << ( -src1[ADDRSIZE-1:0] ) ) | ( src2 >> ( WIDTH -( -src1[ADDRSIZE-1:0] ) ) );
+                    //reg_debug_o=(src1[ADDRSIZE-1:0])?1:0;
             end
             /*9*/`HLT: begin
                 debug_r = 5;
